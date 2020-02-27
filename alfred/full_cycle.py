d00a3e07098667f159ac67dd01e4c8c3ba62811b
JulienRoy <jul.roy1311@gmail.com>
2020-02-27 16:04:08 -0500

    Merged full_cycle.py

--- alfred/full_cycle.py
+++ alfred/full_cycle.py
@@ -29,7 +29,7 @@ def get_full_cycle_args():
     parser.add_argument('--n_experiments_per_proc', type=int, default=np.inf)
     parser.add_argument('--use_pbar', type=parse_bool, default=False)
     parser.add_argument('--check_hash', type=parse_bool, default=True)
-    parser.add_argument('--run_over_envs', type=parse_bool, default=False,
+    parser.add_argument('--run_over_tasks', type=parse_bool, default=False,
                         help="If true, subprocesses will look for unhatched seeds in all storage_dir"
                              "that have the same hashes, 'alg_name', 'desc' but different 'task_name'")
     parser.add_argument('--run_clean_interrupted', type=parse_bool, default=False,
@@ -65,21 +65,22 @@ if __name__ == '__main__':
 
     cycle_logger.info("\nSTARTING PART (2) - Creating retrainBest directories")
 
-    retrainBest_storage_dirs = create_retrain_best(storage_name=full_cycle_args.storage_name, run_over_envs=full_cycle_args.run_over_envs,
-                                                  n_retrain_seeds=full_cycle_args.n_retrain_seeds)
+    retrainBest_storage_dirs = create_retrain_best(storage_name=full_cycle_args.storage_name,
+                                                   run_over_tasks=full_cycle_args.run_over_tasks,
+                                                   n_retrain_seeds=full_cycle_args.n_retrain_seeds)
 
     # 3: RUNNING THE SCHEDULE FOR RETRAIN BESTS ---
 
     cycle_logger.info("\nSTARTING PART (3) - Running run_schdule on retrainBest directories")
 
     # Since different retrain-directories have different descriptions, we cannot simply launch run_schedule with
-    # --run_over_envs=True to launch them all sequentially. We have to launch run_schedule separately on each of
+    # --run_over_tasks=True to launch them all sequentially. We have to launch run_schedule separately on each of
     # the retrain-directory. However, since the number of seeds used for retrain is typically smaller than n_processes
     # we can launch multiple run_schedule in parallel in different subprocesses.
 
     run_schedule_args = deepcopy(vars(full_cycle_args))
     del run_schedule_args['n_retrain_seeds']
-    run_schedule_args['run_over_envs'] = False
+    run_schedule_args['run_over_tasks'] = False
 
     n_parallel_run_schedule = full_cycle_args.n_processes // full_cycle_args.n_retrain_seeds
 
